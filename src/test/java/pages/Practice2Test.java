a49785c7eadb69d01ee7cd8a013c166490d35142
ArvindAspire <arvind6tomar@gmail.com>
2019-11-18 22:59:41 +0530

    just

---
+++ src/test/java/pages/Practice2Test.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.testng.annotations.Test;
+
+public class Practice2Test {
+
+  @Test
+  public void afterMethodTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void beforeMethodTest() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void test1Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+
+  @Test
+  public void test2Test() {
+    throw new RuntimeException("Test not implemented");
+  }
+}
