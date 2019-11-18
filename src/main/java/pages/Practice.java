7ccef7bfab8aea111360e957b990e1d7e8e86482
ArvindAspire <arvind6tomar@gmail.com>
2019-11-19 00:01:30 +0530

    last commit

--- src/main/java/pages/Practice.java
+++ src/main/java/pages/Practice.java
@@ -1,5 +1,28 @@
 package pages;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class Practice {
+	
+@BeforeMethod
+public void  beforeMethod() {
+	
+	System.out.println("before method ");
+	
+}
+
+@Test
+public static void test1() {
+	System.out.println("test case 1 ");
+}
+
+
+
+@AfterMethod
+public void afterMethod() {
+	System.out.println("aftermethod ");
+}
 
 }
--- src/main/java/pages/Practice2.java
+++ src/main/java/pages/Practice2.java
@@ -1,11 +1,18 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Practice2 {
 	
+
 @BeforeMethod
 public void  beforeMethod() {
 	
@@ -13,20 +20,38 @@ public void  beforeMethod() {
 	
 }
 
+@Parameters({"animal"})
 @Test
-public void test1() {
-	System.out.println("test case 1 dog");
+public void test1(String animal) {
+	System.out.println("test case "+animal);
 }
 
-@Test
-public void test2() {
-	System.out.println("test case 2 cat");
+
+@Test(dataProvider="getdata")
+public void test2(String animal) {
+	System.out.println("data is "+animal);
+	Practice.test1();
+	
 }
 
 
 @AfterMethod
 public void afterMethod() {
 	System.out.println("aftermethod dead");
+}
+
+@DataProvider
+public  Object[]  getdata() {
+	Object[] obj=new Object[2];
+	obj[0]="lion";
+	obj[1]="tiger";
+	
+	
+	
+	return obj;
+	
+	
+	
 }
 
 }
